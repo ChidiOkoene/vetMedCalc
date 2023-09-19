bc43e5dd29e7e4e61a91be0b8acbfcd8b0202cab
Chidi Okoene <chidiokoene@gmail.com>
2023-09-19 01:59:35 +0100

    Update dose_calculator.js

--- dose_calculator.js
+++ dose_calculator.js
@@ -4,11 +4,30 @@ document.getElementById("dosageForm").addEventListener("submit", function(event)
     // Retrieve form data
     var selectedDrug = document.getElementById("Drug").value;
     var weight = parseFloat(document.getElementById("Weight").value);
-    var dosage = parseFloat(document.getElementById("dosage").value);
+    var conc = parseFloat(document.getElementById("conc").value);
 
     // Perform dosage calculation and other logic here
+    // myscript.js
+
+    function calculateDosage() {
+    // Get the selected drug value
+        var selectedDrug = document.getElementById("Drug").value;
+
+    // Get the weight entered by the user
+        var weight = parseFloat(document.getElementById("Weight").value);
+
+        var conc = parseFloat(document.getElementById("conc").value);
+
+    // Perform a calculation using the selected drug value and weight
+        var dosage = selectedDrug * conc / weight;
+
+    // Display the result
+        var resultContainer = document.getElementById("result");
+        resultContainer.innerHTML = "Dosage: " + dosage;
+    }
+
    
-    calculatedDosage = selectedDrug * dosage / weight 
+    calculatedDosage = selectedDrug * conc / weight 
 
     // Display the results on the same page
     var resultContainer = document.getElementById("resultContainer");
