a1cad60a4b4935e0f362ce2ac6b062c3dd05449c
Chidi Okoene <chidiokoene@gmail.com>
2023-09-19 01:07:58 +0100

    Update dose_calculator.js

--- dose_calculator.js
+++ dose_calculator.js
@@ -1,1 +1,21 @@
+document.getElementById("dosageForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Retrieve form data
+    var selectedDrug = document.getElementById("Drug").value;
+    var weight = parseFloat(document.getElementById("Weight").value);
+    var dosage = parseFloat(document.getElementById("dosage").value);
+
+    // Perform dosage calculation and other logic here
+    function dose_calculator(selectedDrug, weight, dosage) {
+        
+    }
+
+    // Display the results on the same page
+    var resultContainer = document.getElementById("resultContainer");
+    resultContainer.innerHTML = "Dosage: " + calculatedDosage; // Replace with your actual calculation
+
+    // Optionally, you can submit the form data to a server using AJAX if needed
+});
+
 
